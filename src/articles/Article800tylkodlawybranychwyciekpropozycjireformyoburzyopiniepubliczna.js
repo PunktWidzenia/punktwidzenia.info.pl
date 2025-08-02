7717d29937d5ed00415d00a3ba580d223690f913
Piotr Sawicki <kontaktpunktwidzenia@gmail.com>
2025-08-02 11:49:14 +0200

    poprawka

--- src/articles/Article800tylkodlawybranychwyciekpropozycjireformyoburzyopiniepubliczna.js
+++ src/articles/Article800tylkodlawybranychwyciekpropozycjireformyoburzyopiniepubliczna.js
@@ -36,12 +36,11 @@ function Article800tylkodlawybranychwyciekpropozycjireformyoburzyopiniepubliczna
         <Image
           src={"/800-tylko-dla-wybranych-wyciek-propozycji-reformy-oburzy-opinie-publiczna.webp"}
           alt={"🧨 800+ tylko dla wybranych? Wyciek propozycji reformy oburzył opinię publiczną"}
-            width={1200}
-  height={630}
+          width={1200}
+          height={630}
           loading="lazy"
-          fetchPriority="low"
+          fetchPriority="high"
           className="w-full h-auto rounded"
-          unoptimized
         />
         
         <div className="space-y-6">
