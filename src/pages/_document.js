eff8353faebe3fe16f7e9eabdbde5963895218ec
Piotr Sawicki <kontaktpunktwidzenia@gmail.com>
2025-09-07 18:45:48 +0200

    poprawka

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,16 +4,58 @@ export default function Document() {
   return (
     <Html lang="pl" className="dark">
       <Head>
+        {/* Google Search Console */}
+        <meta
+          name="google-site-verification"
+          content="f2sZHKIc-IX_gcu5cvdN83chUTU8RedqbUvEVxn8FGo"
+        />
+
         {/* Favicony / splash */}
-        <link rel="apple-touch-startup-image" media="(device-width: 320px)" href="/apple-splash-640x1136.png" />
-        <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px)" href="/apple-splash-750x1334.png" />
-        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" href="/apple-splash-828x1792.png" />
-        <link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px)" href="/apple-splash-1125x2436.png" />
-        <link rel="apple-touch-startup-image" media="(device-width: 390px) and (device-height: 844px)" href="/apple-splash-1170x2532.png" />
-        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px)" href="/apple-splash-1242x2208.png" />
-        <link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" href="/apple-splash-1242x2688.png" />
-        <link rel="apple-touch-startup-image" media="(device-width: 428px) and (device-height: 926px)" href="/apple-splash-1284x2778.png" />
-        <link rel="apple-touch-startup-image" media="(device-width: 360px) and (device-height: 780px)" href="/apple-splash-1080x2340.png" />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 320px)"
+          href="/apple-splash-640x1136.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 375px) and (device-height: 667px)"
+          href="/apple-splash-750x1334.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+          href="/apple-splash-828x1792.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 375px) and (device-height: 812px)"
+          href="/apple-splash-1125x2436.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 390px) and (device-height: 844px)"
+          href="/apple-splash-1170x2532.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 414px) and (device-height: 736px)"
+          href="/apple-splash-1242x2208.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+          href="/apple-splash-1242x2688.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 428px) and (device-height: 926px)"
+          href="/apple-splash-1284x2778.png"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          media="(device-width: 360px) and (device-height: 780px)"
+          href="/apple-splash-1080x2340.png"
+        />
 
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -38,8 +80,8 @@ export default function Document() {
   try {
     var KEY = 'pw-theme';
     var pref = localStorage.getItem(KEY); // 'dark' | 'light' | null
- // DOMYŚLNIE CIEMNY. Tylko świadomy wybór 'light' w LS włącza jasny.
- var dark = pref !== 'light';
+    // DOMYŚLNIE CIEMNY. Tylko świadomy wybór 'light' w LS włącza jasny.
+    var dark = pref !== 'light';
 
     var doc = document.documentElement;
     if (dark) doc.classList.add('dark'); else doc.classList.remove('dark');
